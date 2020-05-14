c2c951f0a9600aaf3c09d0f7a988d27cbdf7ea1c
mauronaca <mnacachian8@gmail.com>
2020-05-13 23:49:09 -0300

    Pruebas en main

--- Tp_0/src/main.cpp
+++ Tp_0/src/main.cpp
@@ -13,7 +13,13 @@ using namespace std;
 int main() {
 	Images imagen(10,10,15);
 	int** matriz = imagen.getMatrix();
+	Complejo Z1, Z2;
+	Complejo Z3 = (2,3);
+	Complejo Z4(1,0);
 
+	Z1 = Z2 = Z4;
+	Z1 = Z2 + Z3*Z3; 
+	Z1.printPolar();
 	cout << matriz[1][1] << endl;
 
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
