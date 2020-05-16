2063cc70eb64e584748ccbbb667ef5fcd1ff5886
Elías Urquiza <24704187+GenghiSkeleton@users.noreply.github.com>
2020-05-16 16:06:02 -0300

    algunas cosas sin importancia

--- Tp_0/Debug/src/ComplexPlane.d
+++ Tp_0/Debug/src/ComplexPlane.d
@@ -1,3 +1,8 @@
-src/ComplexPlane.o: ../src/ComplexPlane.cpp ../src/ComplexPlane.h
+src/ComplexPlane.o: ../src/ComplexPlane.cpp ../src/ComplexPlane.h \
+ ../src/Complejo.h ../src/Images.h
 
 ../src/ComplexPlane.h:
+
+../src/Complejo.h:
+
+../src/Images.h:
--- Tp_0/src/ComplexPlane.h
+++ Tp_0/src/ComplexPlane.h
@@ -8,12 +8,25 @@
 #ifndef COMPLEXPLANE_H_
 #define COMPLEXPLANE_H_
 
+#include "Complejo.h"
+#include "Images.h"
+
 class ComplexPlane {
+private:
+	Complejo comp; //número complejo guardado
+	Images image; //imagen
+	int index; //indice del pixel de una imagen (?)
+
 public:
 	ComplexPlane();
 	virtual ~ComplexPlane();
 	ComplexPlane(const ComplexPlane &other);
 	ComplexPlane& operator=(const ComplexPlane &other);
+
+
+	//Complejo index2Comp();
+	//Int comp2Index();
+
 };
 
 #endif /* COMPLEXPLANE_H_ */
